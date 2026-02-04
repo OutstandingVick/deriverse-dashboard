a56643b0b434e7dd6b96ab5bbb349ecfd8aa336c
OutstandingVick <scottdodo2015@outlook.com>
2026-02-04 11:25:42 +0100

    installed router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from 'react-scroll-to-top';
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <ScrollToTop /> */}
+      <div className='min-h-screen dark:bg-dain bg-main bg-center px-5 md:px-20'>
+        <ScrollToTop />
+        <Navbar />
+        <div className='p-0'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
