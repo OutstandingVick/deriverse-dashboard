424169865321c9fb237a5db68e782aa909590208
OutstandingVick <scottdodo2015@outlook.com>
2026-02-06 11:01:46 +0100

    ongoing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from 'react-scroll-to-top';
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import DashboardSummary from './DashboardSummary';
 
 function App() {
   return (
@@ -11,13 +9,11 @@ function App() {
       {/* <ScrollToTop /> */}
       <div className='min-h-screen dark:bg-dain bg-main bg-center px-5 md:px-20'>
         <ScrollToTop />
-        <Navbar />
         <div className='p-0'>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<DashboardSummary />} />
           </Routes>
         </div>
-        <Footer />
       </div>
     </Router>
   );
